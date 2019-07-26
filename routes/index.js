8c7b265d21e3d815b48908839e1a75ace3dd7e3a
nithin <pnithin36@gmail.com>
2019-07-26 07:18:34 +0530

    commit by nithin

--- routes/index.js
+++ routes/index.js
@@ -2,55 +2,54 @@ var express = require('express')
 var app = express()
 var request=require('request');
 var cheerio=require('cheerio');
+/**
+ * Cheerio is for parseing HTML elements
+ * Request is for makeing  http calls
+ */ 
 
-
-app.post('/webscrap', function(req, res, next){	
-	req.assert('url', 'Url is required').notEmpty()           //Validate name
+app.post('/webscrap', function(req, res, next)
+{	
+    //Validate post element url
+    req.assert('url', 'Url is required').notEmpty()           
 	
     var errors = req.validationErrors()
     
-    if( !errors ) {   //No errors were found.  Passed Validation!
+    if( !errors ) 
+    {   //No errors were found.  Passed Validation!
 
-	     var title = "";
-      var metaDescription = "";
+	var title = "";
+        var metaDescription = "";
         var results = [];
-     var url=req.sanitize('url').trim();
+        var url=req.sanitize('url').trim();
  	request(url,function(error,response,html)
-
- {
+       {
      
             if(!error)
-    {
-     	var $=cheerio.load(html);
+              {
+     	        var $=cheerio.load(html);
+                title = $("title").text();
+                metaDescription =  $('meta[name=description]').attr("content");
+                $("img").each(function(i, image) 
+                {
+         // resolve absolute image URL and add to our results array
+                   results.push($(image).attr('src'));
+                });
+                //Showing the result in json format
+                res.json({ title: title,description:metaDescription,images:results })
+              }
+              else
+                  //Showing error in json format
+                res.json({error:error});	
+        });
 
-     title = $("title").text();
-      metaDescription =  $('meta[name=description]').attr("content");
-      $("img").each(function(i, image) {
 
-//         // 6. resolve absolute URL and add to our results array
-         results.push($(image).attr('src'));
-     });
- res.json({ title: title,description:metaDescription,images:results })
-     }
-     else
-     res.json({error:error});	
- });
+    }
+    else 
+    {   
+        // Showing validation errors
+	res.json(errors);
 
-
-	}
-	else {   //Display errors to user
-		// var error_msg = ''
-		// errors.forEach(function(error) {
-		// 	error_msg += error.msg + '<br>'
-		// })				
-		//req.flash('error', error_msg)		
-		res.json(errors);
-
-		/**
-		 * Using req.body.name 
-		 * because req.param('name') is deprecated
-		 */ 
-     
+		
     }
 })
 /** 
